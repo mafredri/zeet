0dc4439eba849a12d677fa1e623d9bbdaf123048
Mathias Fredriksson <mafredri@gmail.com>
2016-01-24 17:44:55 +0200

    Bindkey for PgUp and PgDown, re-enable zsh-hss

--- zeet.zsh
+++ zeet.zsh
@@ -69,34 +69,29 @@ export CLICOLOR="yes"
 # to add other keys to this hash, see: man 5 terminfo
 typeset -A key
 
-key[Home]=${terminfo[khome]}
-key[End]=${terminfo[kend]}
-key[Insert]=${terminfo[kich1]}
-key[Delete]=${terminfo[kdch1]}
-# key[Up]='^[[A'
-# key[Down]='^[[B'
-key[Up]=${terminfo[kcuu1]}
-key[Down]=${terminfo[kcud1]}
-key[Left]=${terminfo[kcub1]}
-key[Right]=${terminfo[kcuf1]}
-key[PageUp]=${terminfo[kpp]}
-key[PageDown]=${terminfo[knp]}
-key[ShiftTab]=${terminfo[kcbt]}
+key[Home]=$terminfo[khome]
+key[End]=$terminfo[kend]
+key[Insert]=$terminfo[kich1]
+key[Delete]=$terminfo[kdch1]
+key[Up]=$terminfo[kcuu1]
+key[Down]=$terminfo[kcud1]
+key[Left]=$terminfo[kcub1]
+key[Right]=$terminfo[kcuf1]
+key[PageUp]=$terminfo[kpp]
+key[PageDown]=$terminfo[knp]
+key[ShiftTab]=$terminfo[kcbt]
 
-# setup key accordingly
-[[ -n "${key[Home]}"     ]] && bindkey "${key[Home]}"     beginning-of-line
-[[ -n "${key[End]}"      ]] && bindkey "${key[End]}"      end-of-line
-[[ -n "${key[Insert]}"   ]] && bindkey "${key[Insert]}"   overwrite-mode
-[[ -n "${key[Delete]}"   ]] && bindkey "${key[Delete]}"   delete-char
-# [[ -n "${key[Up]}"       ]] && bindkey "${key[Up]}"       history-substring-search-up
-# [[ -n "${key[Down]}"     ]] && bindkey "${key[Down]}"     history-substring-search-down
-# [[ -n "${key[Up]}"       ]] && bindkey "${key[Up]}"       history-incremental-search-backward
-# [[ -n "${key[Down]}"     ]] && bindkey "${key[Down]}"     history-incremental-search-forward
-[[ -n "${key[Left]}"     ]] && bindkey "${key[Left]}"     backward-char
-[[ -n "${key[Right]}"    ]] && bindkey "${key[Right]}"    forward-char
-# [[ -n "${key[PageUp]}"   ]] && bindkey "${key[PageUp]}"   beginning-of-buffer-or-history
-# [[ -n "${key[PageDown]}" ]] && bindkey "${key[PageDown]}" end-of-buffer-or-history
-[[ -n "${key[ShiftTab]}" ]] && bindkey "${key[ShiftTab]}" reverse-menu-complete
+[[ -n $key[Home]     ]] && bindkey $key[Home]     beginning-of-line
+[[ -n $key[End]      ]] && bindkey $key[End]      end-of-line
+[[ -n $key[Insert]   ]] && bindkey $key[Insert]   overwrite-mode
+[[ -n $key[Delete]   ]] && bindkey $key[Delete]   delete-char
+[[ -n $key[Up]       ]] && bindkey $key[Up]       history-substring-search-up
+[[ -n $key[Down]     ]] && bindkey $key[Down]     history-substring-search-down
+[[ -n $key[Left]     ]] && bindkey $key[Left]     backward-char
+[[ -n $key[Right]    ]] && bindkey $key[Right]    forward-char
+[[ -n $key[PageUp]   ]] && bindkey $key[PageUp]   up-line-or-history
+[[ -n $key[PageDown] ]] && bindkey $key[PageDown] down-line-or-history
+[[ -n $key[ShiftTab] ]] && bindkey $key[ShiftTab] reverse-menu-complete
 
 # [Ctrl-r] - Reverse search
 bindkey '^r' history-incremental-search-backward
@@ -105,6 +100,7 @@ bindkey '^f' history-incremental-search-forward
 # [Esc-e] - Edit command line in $EDITOR
 autoload -U edit-command-line && zle -N edit-command-line
 bindkey '\ee' edit-command-line
+
 # [Esc-w] - Kill from the cursor to the beginning of line
 bindkey '\ew' kill-region
 
@@ -115,14 +111,16 @@ bindkey '\e[1;9D' backward-word
 
 # Finally, make sure the terminal is in application mode, when zle is
 # active. Only then are the values from $terminfo valid.
-function zle-line-init () {
-        echoti smkx
-}
-function zle-line-finish () {
-        echoti rmkx
-}
-zle -N zle-line-init
-zle -N zle-line-finish
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+	function zle-line-init() {
+		echoti smkx
+	}
+	function zle-line-finish() {
+		echoti rmkx
+	}
+	zle -N zle-line-init
+	zle -N zle-line-finish
+fi
 
 # Source a local zshrc if available
 [[ -e ~/.zshrc.local ]] && source ~/.zshrc.local
@@ -138,5 +136,5 @@ $ZSH/extra.zsh &!
 prompt pure
 
 source $ZSH/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-# source $ZSH/modules/zsh-history-substring-search/zsh-history-substring-search.zsh
+source $ZSH/modules/zsh-history-substring-search/zsh-history-substring-search.zsh
 source $ZSH/modules/z/z.sh
