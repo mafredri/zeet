d8584c7dfaaf9ef5124b5e1dc9f2f957a61f3a94
Mathias Fredriksson <mafredri@gmail.com>
2016-07-05 06:59:04 +0300

    Bindkey for z-h-s-s after it has been sourced
    If they're bound earlier, z-sy-h will not be able to find them when it
     is sourced, as they haven't been defined yet.

--- zeet.zsh
+++ zeet.zsh
@@ -85,8 +85,6 @@ key[ShiftTab]=$terminfo[kcbt]
 [[ -n $key[End]      ]] && bindkey $key[End]      end-of-line
 [[ -n $key[Insert]   ]] && bindkey $key[Insert]   overwrite-mode
 [[ -n $key[Delete]   ]] && bindkey $key[Delete]   delete-char
-[[ -n $key[Up]       ]] && bindkey $key[Up]       history-substring-search-up
-[[ -n $key[Down]     ]] && bindkey $key[Down]     history-substring-search-down
 [[ -n $key[Left]     ]] && bindkey $key[Left]     backward-char
 [[ -n $key[Right]    ]] && bindkey $key[Right]    forward-char
 [[ -n $key[PageUp]   ]] && bindkey $key[PageUp]   up-line-or-history
@@ -138,4 +136,8 @@ prompt pure
 
 source $ZSH/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $ZSH/modules/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+[[ -n $key[Up]   ]] && bindkey $key[Up]   history-substring-search-up
+[[ -n $key[Down] ]] && bindkey $key[Down] history-substring-search-down
+
 source $ZSH/modules/z/z.sh
