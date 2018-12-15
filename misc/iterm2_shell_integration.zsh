815a438ea80104819b84366c98f61161e80c27b6
Mathias Fredriksson <mafredri@gmail.com>
2018-12-15 23:51:46 +0200

    misc: Update iterm2 shell integration to v7

--- misc/iterm2_shell_integration.zsh
+++ misc/iterm2_shell_integration.zsh
@@ -1,3 +1,17 @@
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 if [[ -o interactive ]]; then
   if [ "$ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX""$TERM" != "screen" -a "$ITERM_SHELL_INTEGRATION_INSTALLED" = "" ]; then
     ITERM_SHELL_INTEGRATION_INSTALLED=Yes
@@ -89,7 +103,7 @@ if [[ -o interactive ]]; then
       ITERM2_SHOULD_DECORATE_PROMPT=""
 
       # Add our escape sequences just before the prompt is shown.
-      if [[ $PS1 == *'$(iterm2_prompt_mark)'* ]]
+      if [[ $PS1 == *"$(iterm2_prompt_mark)"* ]]
       then
         PS1="$PS1%{$(iterm2_prompt_end)%}"
       else
@@ -133,6 +147,6 @@ if [[ -o interactive ]]; then
     preexec_functions=($preexec_functions iterm2_preexec)
 
     iterm2_print_state_data
-    printf "\033]1337;ShellIntegrationVersion=6;shell=zsh\007"
+    printf "\033]1337;ShellIntegrationVersion=7;shell=zsh\007"
   fi
 fi
