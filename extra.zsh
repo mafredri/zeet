c64c3e9b683559b0d13216d97fa110389f395cdf
Mathias Fredriksson <mafredri@gmail.com>
2020-09-25 11:43:50 +0300

    Load zsh/stat as zstat

--- extra.zsh
+++ extra.zsh
@@ -8,7 +8,7 @@
 compile_zcompdump() {
 	setopt local_options null_glob extended_glob no_sh_word_split
 
-	zmodload zsh/stat
+	zmodload -F zsh/stat b:zstat
 	# Make sure zcompdump files have been compiled
 	for zcd in ~/.zcompdump*~*.zwc; do
 		if (( $(zstat +mtime $zcd) > $(zstat +mtime $zcd.zwc 2>/dev/null || print 0) )); then
