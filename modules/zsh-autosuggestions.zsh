ecd96954df73b29fece130e134e5dc559a0dfbbd
Mathias Fredriksson <mafredri@gmail.com>
2020-09-30 11:22:33 +0300

    Clean up zsh-autosuggestions init

--- modules/zsh-autosuggestions.zsh
+++ modules/zsh-autosuggestions.zsh
@@ -1,22 +1,10 @@
 #!/usr/bin/env zsh
 
+source $ZSH/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
+
 # Preventing async is part of the fix to prevent zsh-asug form causing
 # havoc on every precmd. If enabled, the "server" will be recreated on
 # every prompt.
 unset ZSH_AUTOSUGGEST_USE_ASYNC
-unset ZSH_AUTOSUGGEST_MANUAL_REBIND
-
-source $ZSH/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-# This hook replaces _zsh_autosuggest_start so that we can remove the
-# hook for rebinding widgets on every prompt...
-_custom_zsh_autosuggest_start() {
-	add-zsh-hook -d precmd _custom_zsh_autosuggest_start
-
-	_zsh_autosuggest_start
-	add-zsh-hook -d precmd _zsh_autosuggest_bind_widgets
-}
-
 # Prevent zsh-autosuggestions from rebinding widgets on every prompt.
-add-zsh-hook -d precmd _zsh_autosuggest_start
-add-zsh-hook precmd _custom_zsh_autosuggest_start
+typeset -g ZSH_AUTOSUGGEST_MANUAL_REBIND=
