863ee746aa156359abec20447232b64f945b582d
Mathias Fredriksson <mafredri@gmail.com>
2021-10-31 15:24:31 +0200

    misc: Implement restore-prompt after halloween

--- misc/pure_halloween.zsh
+++ misc/pure_halloween.zsh
@@ -38,13 +38,35 @@ pure_set_halloween_color_scheme() {
 	zstyle :prompt:pure:virtualenv          color $pure_theme[color6]
 }
 
+pure_unset_halloween_color_scheme() {
+	# TODO(mafredri): Restore previous theme instead of resetting to defaults.
+	zstyle -d :prompt:pure:execution_time      color
+	zstyle -d :prompt:pure:git:arrow           color
+	zstyle -d :prompt:pure:git:branch          color
+	zstyle -d :prompt:pure:git:branch:cached   color
+	zstyle -d :prompt:pure:git:dirty           color
+	zstyle -d :prompt:pure:host                color
+	zstyle -d :prompt:pure:path                color
+	zstyle -d :prompt:pure:prompt:error        color
+	zstyle -d :prompt:pure:prompt:success      color
+	zstyle -d :prompt:pure:user                color
+	zstyle -d :prompt:pure:user:root           color
+	zstyle -d :prompt:pure:virtualenv          color
+}
+
 integer _pure_apply_custom_theme_init
+typeset -A pure_apply_custom_theme_restore
 pure_apply_custom_theme() {
 	typeset -A current_date
 	current_date=($(date +'month '%m' day '%d))
 	if (( current_date[month] == 10 )) && (( current_date[day] >= 20 )); then
+		pure_apply_custom_theme_restore[unset_theme]=pure_unset_halloween_color_scheme
 		pure_set_halloween_color_scheme ${(kv)pure_halloween_kiss_color_scheme}
 		if (( current_date[day] == 31 )); then
+			pure_apply_custom_theme_restore+=(
+				symbol "$PURE_PROMPT_SYMBOL"
+				rprompt "$RPROMPT"
+			)
 			if [[ -z $TMUX ]]; then
 				# 🧛🏻‍♀️
 				RPROMPT=$'%2{\U1F9DB\U1F3FB\U200D\U2640\UFE0F%}'
@@ -56,6 +78,19 @@ pure_apply_custom_theme() {
 				PURE_PROMPT_SYMBOL=$'%2{\U1F9DB%} ❯'
 			fi
 		fi
+	else
+		$pure_apply_custom_theme_restore[unset_theme]
+		if [[ -n $pure_apply_custom_theme_restore[symbol] ]]; then
+			PURE_PROMPT_SYMBOL=$pure_apply_custom_theme_restore[symbol]
+		else
+			unset PURE_PROMPT_SYMBOL
+		fi
+		if [[ -n $pure_apply_custom_theme_restore[rprompt] ]]; then
+			RPROMPT=$pure_apply_custom_theme_restore[rprompt]
+		else
+			unset RPROMPT
+		fi
+		pure_apply_custom_theme_restore=()
 	fi
 	if ((!_pure_apply_custom_theme_init)); then
 		_pure_apply_custom_theme_init=1
