019a038d33dbdd96cbfc1a8a38598f6195e2a434
Mathias Fredriksson <mafredri@gmail.com>
2022-06-26 23:48:46 +0300

    Update direnv hook

--- modules/direnv.zsh
+++ modules/direnv.zsh
@@ -1,14 +1,16 @@
+#!/usr/bin/env zsh
 
+# Output from: direnv hook zsh
 _direnv_hook() {
-  trap -- '' SIGINT;
-  eval "$("direnv" export zsh)";
-  trap - SIGINT;
+	trap -- '' SIGINT;
+	eval "$(direnv export zsh)";
+	trap - SIGINT;
 }
 typeset -ag precmd_functions;
-if [[ -z ${precmd_functions[(r)_direnv_hook]} ]]; then
-  precmd_functions=( _direnv_hook ${precmd_functions[@]} )
+if [[ -z "${precmd_functions[(r)_direnv_hook]+1}" ]]; then
+	precmd_functions=( _direnv_hook ${precmd_functions[@]} )
 fi
 typeset -ag chpwd_functions;
-if [[ -z ${chpwd_functions[(r)_direnv_hook]} ]]; then
-  chpwd_functions=( _direnv_hook ${chpwd_functions[@]} )
+if [[ -z "${chpwd_functions[(r)_direnv_hook]+1}" ]]; then
+	chpwd_functions=( _direnv_hook ${chpwd_functions[@]} )
 fi
